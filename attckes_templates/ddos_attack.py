9edceb7d66326714e9cf8ee53c37011ec53ccd84
0Xdarkday <95956397+0Xdarkday@users.noreply.github.com>
2024-05-26 15:48:41 +0300

    ddos_attack.py

--- attckes_templates/ddos_attack.py
+++ attckes_templates/ddos_attack.py
@@ -1,22 +1,23 @@
 from datetime import datetime, timedelta
 from collections import defaultdict
-from .base_attack import BaseAttack
+from base_attack import BaseAttack
 
 class DDOSAttackDetector(BaseAttack):
     def __init__(self, reporter, config):
         super().__init__(reporter)
         self.ddos_tracker = defaultdict(list)
-        self.ddos_threshold = config['detection_thresholds']['ddos']['threshold']
-        self.ddos_window = timedelta(seconds=config['detection_thresholds']['ddos']['window'])
+        self.threshold = config['threshold']
+        self.window = timedelta(seconds=config['window'])
 
     def detect(self, packet):
         if hasattr(packet, 'ip'):
             src_ip = packet.ip.src
-            current_time = datetime.now()
-            self.ddos_tracker[src_ip].append(current_time)
+            now = datetime.now()
+            self.ddos_tracker[src_ip].append(now)
 
-            # Remove entries outside the time window
-            self.ddos_tracker[src_ip] = [time for time in self.ddos_tracker[src_ip] if current_time - time < self.ddos_window]
-
-            if len(self.ddos_tracker[src_ip]) > self.ddos_threshold:
-                self.reporter.report_attack("DDoS Atta
+            # Remove outdated entries
+            self.ddos_tracker[src_ip] = [time for time in self.ddos_tracker[src_ip] if now - time <= self.window]
+            
+            if len(self.ddos_tracker[src_ip]) >= self.threshold:
+                self.reporter.report_attack('ddos', src_ip, {'occurrences': len(self.ddos_tracker[src_ip])})
+                self.ddos_tracker[src_ip].clear()
