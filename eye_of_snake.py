7f0137a225c7e82763c94a156a523f01a99dbe3d
0Xdarkday <95956397+0Xdarkday@users.noreply.github.com>
2024-05-26 22:53:11 +0300

    Update eye_of_snake.py

--- eye_of_snake.py
+++ eye_of_snake.py
@@ -26,8 +26,5 @@ def main():
     # Start packet capture
     start_packet_capture(config['interfaces']['default'], packet_filter)
 
-    # Generate HTML report
-    reporter.generate_html_report('network_security_report.html')
-
 if __name__ == '__main__':
     main()
