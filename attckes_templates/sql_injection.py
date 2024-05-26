a9c267a1de0f50265e0951c5f5abc86708a2e498
0Xdarkday <95956397+0Xdarkday@users.noreply.github.com>
2024-05-26 19:21:41 +0300

    Update sql_injection.py

--- attckes_templates/sql_injection.py
+++ attckes_templates/sql_injection.py
@@ -1,14 +1,14 @@
-from base_attack import BaseAttack
+from .base_attack import BaseAttack  # Use relative import
 
 class SQLInjectionDetector(BaseAttack):
-    def __init__(self, reporter, patterns):
+    def __init__(self, reporter, config):
         super().__init__(reporter)
-        self.patterns = patterns
+        self.patterns = config['patterns']['sql_injection']
 
     def detect(self, packet):
         if hasattr(packet, 'http') and hasattr(packet.http, 'request_uri'):
             request_uri = packet.http.request_uri
             for pattern in self.patterns:
                 if pattern in request_uri:
-                    self.reporter.report_attack('sql_injection', packet.ip.src, {'uri': request_uri, 'pattern': pattern})
+                    self.reporter.report_attack('SQL Injection', packet.ip.src, {'uri': request_uri, 'pattern': pattern})
                     break
