65ce121885341d84b7ec4d75ff5b7160c19f4e55
0Xdarkday <95956397+0Xdarkday@users.noreply.github.com>
2024-05-26 19:48:44 +0300

    Delete attckes_templates/base_attack.py

--- attckes_templates/base_attack.py
+++
@@ -1,9 +0,0 @@
-class BaseAttack:
-    def __init__(self, reporter):
-        self.reporter = reporter
-
-    def detect(self, packet):
-        """
-        Detects an attack. Should be implemented by subclasses.
-        """
-        raise NotImplementedError("Subclasses should implement this method")
