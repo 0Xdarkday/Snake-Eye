a171ea4aab270654b7c6794caf9ae533c191fde3
0Xdarkday <95956397+0Xdarkday@users.noreply.github.com>
2024-05-26 19:22:07 +0300

    Update port_scan.py

--- attckes_templates/port_scan.py
+++ attckes_templates/port_scan.py
@@ -1,27 +1,22 @@
 from datetime import datetime, timedelta
 from collections import defaultdict
-from base_attack import BaseAttack
+from .base_attack import BaseAttack  # Use relative import
 
 class PortScanDetector(BaseAttack):
     def __init__(self, reporter, config):
         super().__init__(reporter)
-        self.port_scan_tracker = defaultdict(lambda: defaultdict(list))
-        self.threshold = config['threshold']
-        self.window = timedelta(seconds=config['window'])
+        self.port_scan_tracker = defaultdict(lambda: defaultdict(int))
+        self.port_scan_threshold = config['detection_thresholds']['port_scan']['threshold']
+        self.port_scan_window = timedelta(seconds=config['detection_thresholds']['port_scan']['window'])
 
     def detect(self, packet):
         if packet.transport_layer == 'TCP' and hasattr(packet, 'ip'):
             src_ip = packet.ip.src
             dst_port = packet.tcp.dstport
-            now = datetime.now()
-            self.port_scan_tracker[src_ip][dst_port].append(now)
+            self.port_scan_tracker[src_ip][dst_port] += 1
 
-            # Remove outdated entries
-            for port in list(self.port_scan_tracker[src_ip].keys()):
-                self.port_scan_tracker[src_ip][port] = [time for time in self.port_scan_tracker[src_ip][port] if now - time <= self.window]
-                if not self.port_scan_tracker[src_ip][port]:
-                    del self.port_scan_tracker[src_ip][port]
-
-            if len(self.port_scan_tracker[src_ip]) >= self.threshold:
-                self.reporter.report_attack('port_scan', src_ip, self.port_scan_tracker[src_ip])
-                self.port_scan_tracker[src_ip].clear()
+            if len(self.port_scan_tracker[src_ip]) >= self.port_scan_threshold:
+                first_attempt_time = min(self.port_scan_tracker[src_ip], key=self.port_scan_tracker[src_ip].get)
+                if datetime.now() - first_attempt_time > self.port_scan_window:
+                    self.reporter.report_attack('Port Scan', src_ip, self.port_scan_tracker[src_ip])
+                    self.port_scan_tracker[src_ip].clear()
