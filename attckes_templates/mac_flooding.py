319913d2f463ec6b8bf06190a0f8d52c9a7c3bf8
0Xdarkday <95956397+0Xdarkday@users.noreply.github.com>
2024-05-26 19:20:23 +0300

    mac_flooding.py

--- attckes_templates/mac_flooding.py
+++ attckes_templates/mac_flooding.py
@@ -5,19 +5,16 @@ from base_attack import BaseAttack
 class MACFloodingDetector(BaseAttack):
     def __init__(self, reporter, config):
         super().__init__(reporter)
-        self.mac_tracker = defaultdict(list)
-        self.threshold = config['threshold']
-        self.window = timedelta(seconds=config['window'])
+        self.mac_flooding_tracker = defaultdict(int)
+        self.mac_flooding_threshold = config['detection_thresholds']['mac_flooding']['threshold']
+        self.mac_flooding_window = timedelta(seconds=config['detection_thresholds']['mac_flooding']['window'])
 
     def detect(self, packet):
         if hasattr(packet, 'eth'):
             src_mac = packet.eth.src
-            now = datetime.now()
-            self.mac_tracker[src_mac].append(now)
+            self.mac_flooding_tracker[src_mac] += 1
 
-            # Remove outdated entries
-            self.mac_tracker[src_mac] = [time for time in self.mac_tracker[src_mac] if now - time <= self.window]
-
-            if len(self.mac_tracker[src_mac]) >= self.threshold:
-                self.reporter.report_attack('mac_flooding', src_mac, {'occurrences': len(self.mac_tracker[src_mac])})
-                self.mac_tracker[src_mac].clear()
+            if self.mac_flooding_tracker[src_mac] >= self.mac_flooding_threshold:
+                first_attempt_time = datetime.now() - self.mac_flooding_window
+                self.reporter.report_attack('mac_flooding', src_mac, {'count': self.mac_flooding_tracker[src_mac]})
+                self.mac_flooding_tracker[src_mac] = 0
